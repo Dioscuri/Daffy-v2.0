930eb196d58b8d857b8ab5ec2f49a18e81936bf4
Kalico <parvus.bot@gmail.com>
2022-07-31 13:47:01 -0700

    Discord Command Reload + Refactoring

--- functions.js
+++ functions.js
@@ -4,10 +4,11 @@ require("discord.js");
 
  //Fetches Data from a Sheet in a Spreadsheet
 module.exports.fetchSheetData = async (auth, sheetID, seekRange) =>{
+    console.log("Fetching Data")
     const sheets = google.sheets({ version: 'v4', auth });
     const parameters = {spreadsheetId: sheetID, range:seekRange}
 
-    let result = sheets.spreadsheets.values.get(parameters)
+    const result = await sheets.spreadsheets.values.get(parameters)
         .then(res => { 
             return res.data.values
         })
@@ -15,14 +16,13 @@ module.exports.fetchSheetData = async (auth, sheetID, seekRange) =>{
     return result
  }
 
- 
  module.exports.profile = (receivedMessage, arguments) =>{
     const character = arguments
     if (character.role == "Servant"){                 
         const profileembed = {
             color: 0x0099ff,
-            title : character.timeline + ' ' + character.role + " (" + character.class_container + "): ||"+ character.trueName +"||",
-            description:`*Alignment: ${character.alignment ? character.alignment : "???"}*\n*Height: ${character.height ? `${character.height} cm` : "???"}*\n*Weight: ${character.weight ? `${character.weight} kg` : "???"}*\n*Attribute: ${character.attribute ? `${character.attribute}` : "???"}*`,
+            title : character.timeline + ' ' + character.role + " (" + character.name + "): ||"+ character.trueName +"||",
+            description:`*Alignment: ${character.alignment || "???"}*\n*Height: ${character.height || "???"} cm*\n*Weight: ${character.weight || "???"} kg*\n*Attribute: ${character.attribute || "???"}*`,
 
             thumbnail: {url: character.image,},
             fields: [
@@ -30,16 +30,16 @@ module.exports.fetchSheetData = async (auth, sheetID, seekRange) =>{
                 value: '\u200b',
                 inline: false,},
 
-                { name: `STR: `, value: character.str ? character.str : 'N/A', inline: true,},
-                { name: `END: `, value: character.end ? character.end : 'N/A', inline: true,},
-                { name: `AGI: `, value: character.agi ? character.agi : 'N/A', inline: true,},
+                { name: `STR: `, value: character.str || 'N/A', inline: true,},
+                { name: `END: `, value: character.end || 'N/A', inline: true,},
+                { name: `AGI: `, value: character.agi || 'N/A', inline: true,},
 
                 {name: '\u200b',
                 value: '\u200b',
                 inline: false,},
 
-                { name: `MAG: `, value: character.mag ? character.mag : 'N/A', inline: true,},
-                { name: `LCK: `, value: character.lck ? character.lck : 'N/A', inline: true,},
+                { name: `MAG: `, value: character.mag || 'N/A', inline: true,},
+                { name: `LCK: `, value: character.lck || 'N/A', inline: true,},
                 { name: `NP: `, value: character.np && character.class_container != 'LBLancer'? character.np : '???', inline: true,},
             ]
         }
@@ -50,7 +50,7 @@ module.exports.fetchSheetData = async (auth, sheetID, seekRange) =>{
         const profileembed = {
             color: 0x0099ff,
             title : character.role == "Master" ? `(${character.role} of ${character.servantClass}) -- ${character.fullName}` : `${character.fullName} `,
-            description: `*Alignment: ${character.alignment ? character.alignment : "???"}*\n*Height: ${character.height ? `${character.height} cm` : "???"}*\n*Weight: ${character.weight ? `${character.weight} kg` : "???"}*\n*Age: ${character.age ? `${character.age} years` : "???"}*`,
+            description: `*Alignment: ${character.alignment || "???"}*\n*Height: ${character.height || "???"} cm*\n*Weight: ${character.weight || "???"} kg*\n*Age: ${character.age || "???"} years*`,
 
             thumbnail: {url: character.image,},
             fields: [
@@ -58,29 +58,28 @@ module.exports.fetchSheetData = async (auth, sheetID, seekRange) =>{
                 value: '\u200b',
                 inline: false,},
 
-                { name: `STR: `, value: character.str ? character.str : 'N/A', inline: true,},
-                { name: `END: `, value: character.end ? character.end : 'N/A', inline: true,},
-                { name: `AGI: `, value: character.agi ? character.agi : 'N/A', inline: true,},
+                { name: `STR: `, value: character.str ||'N/A', inline: true,},
+                { name: `END: `, value: character.end || 'N/A', inline: true,},
+                { name: `AGI: `, value: character.agi || 'N/A', inline: true,},
 
                 {name: '\u200b',
                 value: '\u200b',
                 inline: false,},
 
-                { name: `MAG: `, value: character.mag ? character.mag : 'N/A', inline: true,},
-                { name: `LCK: `, value: character.lck ? character.lck : 'N/A', inline: true,},
+                { name: `MAG: `, value: character.mag || 'N/A', inline: true,},
+                { name: `LCK: `, value: character.lck || 'N/A', inline: true,},
 
                 {name: '\u200b',
                 value: '\u200b',
                 inline: false,},
 
-                { name: `INT: `, value: character.int ? character.int : 'N/A', inline: true,},
-                { name: `CHAR: `, value: character.char ? character.char : 'N/A', inline: true,},
+                { name: `INT: `, value: character.int || 'N/A', inline: true,},
+                { name: `CHAR: `, value: character.char || 'N/A', inline: true,},
             ]
         }
         
         receivedMessage.channel.send({embeds:[profileembed]})
     }
 }
-
 
 
